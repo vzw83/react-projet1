5a44e7e32be262f0fb87331deee79b6a41a916c2
vzw83 <vzw83@yandex.ru>
2023-03-22 10:04:33 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://www.kubachiserebro.ru/images/site/cache/02/a7/02a70d5b74169f6daa4f1cef013961f3.jpg" />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div className="content__img">
+        <img src="https://img1.goodfon.ru/original/800x480/c/15/yosemite-national-park-ssha.jpg" />
+      </div>
+      <div>ava + description</div>
+      <div>
+        my posts
+        <div>new posts</div>
+      </div>
+      <div>
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
